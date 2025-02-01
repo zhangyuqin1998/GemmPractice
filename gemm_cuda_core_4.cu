7c72aa8e6d6eedafe17769c74613288fba9aa5e3
zhangyuqin1998 <zhangyuqin@baidu.com>
2025-02-01 15:51:09 +0800

    fix swizzle

--- gemm_cuda_core_4.cu
+++ gemm_cuda_core_4.cu
@@ -6,9 +6,22 @@ constexpr uint64_t TILE_SIZE = 16;
 
 
 template <class T>
-__device__ void LDVectorized(float *dst, const float *src) {
+__device__ __inline__ void LDVectorized(float *dst, const float *src) {
     *(reinterpret_cast<T*>(dst)) = *(reinterpret_cast<const T*>(src));
 }
+
+template <int BBits, int MBase, int SShift = BBits>
+struct Swizzle
+{
+    __device__ __inline__ static uint64_t apply(uint64_t x, uint64_t y, uint64_t rows, uint64_t cols) {
+        uint64_t offset = x + y * cols;
+        uint64_t one = 1;
+        uint64_t bit_msk = (one << BBits) - one;
+        uint64_t yyy_msk = bit_msk << (MBase + SShift);
+        uint64_t msk_sft = SShift;
+        return offset ^ ((offset & yyy_msk) >> msk_sft);;
+    }
+};
 
 __global__ void Kernel(const float *A, const float *B, float *C, uint64_t m, uint64_t n, uint64_t k) {
     __shared__ float s_A[TILE_SIZE][TILE_SIZE];
@@ -20,10 +33,12 @@ __global__ void Kernel(const float *A, const float *B, float *C, uint64_t m, uin
     float sum = 0.0f;
     for (uint64_t k_tile = 0; k_tile < k; k_tile += TILE_SIZE) {
         if (threadIdx.x < TILE_SIZE / 4) {
-            LDVectorized<float4>(&(s_A[threadIdx.y][threadIdx.x * 4]), &(A[y * k + k_tile + threadIdx.x * 4]));
+            uint64_t new_offset = Swizzle<3, 2, 3>::apply(threadIdx.x * 4, threadIdx.y, TILE_SIZE, TILE_SIZE);
+            LDVectorized<float4>(&(s_A[new_offset / TILE_SIZE][new_offset % TILE_SIZE]), &(A[y * k + k_tile + threadIdx.x * 4]));
         }
         if (threadIdx.x < TILE_SIZE / 4) {
-            LDVectorized<float4>(&(s_B[threadIdx.y][threadIdx.x * 4]), &(B[(blockIdx.x * blockDim.x + threadIdx.y) * k + k_tile + threadIdx.x * 4]));
+            uint64_t new_offset = Swizzle<3, 2, 3>::apply(threadIdx.x * 4, threadIdx.y, TILE_SIZE, TILE_SIZE);
+            LDVectorized<float4>(&(s_B[new_offset / TILE_SIZE][new_offset % TILE_SIZE]), &(B[(blockIdx.x * blockDim.x + threadIdx.y) * k + k_tile + threadIdx.x * 4]));
         }
         __syncthreads();
 
@@ -32,8 +47,10 @@ __global__ void Kernel(const float *A, const float *B, float *C, uint64_t m, uin
             for (uint64_t t = 0; t < TILE_SIZE; t += 4) {
                 float f_A[4];
                 float f_B[4];
-                LDVectorized<float4>(f_A, &(s_A[threadIdx.y][t])); 
-                LDVectorized<float4>(f_B, &(s_B[threadIdx.x][t]));
+                uint64_t new_offset_sa = Swizzle<3, 2, 3>::apply(t, threadIdx.y, TILE_SIZE, TILE_SIZE);
+                uint64_t new_offset_sb = Swizzle<3, 2, 3>::apply(t, threadIdx.x, TILE_SIZE, TILE_SIZE);
+                LDVectorized<float4>(f_A, &(s_A[new_offset_sa / TILE_SIZE][new_offset_sa % TILE_SIZE])); 
+                LDVectorized<float4>(f_B, &(s_B[new_offset_sb / TILE_SIZE][new_offset_sb % TILE_SIZE])); 
 #pragma unroll 4
                 for (uint64_t i = 0; i < 4; ++i) {
                     sum += f_A[i] * f_B[i];
--- visualize_swizzle.py
+++ visualize_swizzle.py
@@ -21,6 +21,35 @@ class Swizzle:
         new_offset = self.apply(offset)
         return (new_offset % cols, new_offset // cols)
 
+    def apply_swizzle_v2(self, x, y, rows, cols):
+        #等价V1, 但高效一点
+        offset = x + y * cols
+        element_per_row = 1 << (self.S + self.M)
+        x = offset & (element_per_row - 1)  # 等价于 offset % (2 ** (self.S + self.M))
+        y = offset >> (self.S + self.M)              # 等价于 offset // (2 ** (self.S + self.M))
+
+        offset = x & ((1 << self.M) - 1)             # 等价于 x % (2 ** self.M)
+        x = x >> self.M                              # 等价于 x // (2 ** self.M)
+        x = y ^ x
+        x = (x << self.M) | offset                   # 等价于 x * (2 ** self.M) + offset
+        x = x & (element_per_row - 1)       # 等价于 x % (2 ** (self.S + self.M))
+        new_offset = x + (y << (self.S + self.M))    # 等价于 x + y * (2 ** (self.S + self.M))
+        return (new_offset % cols, new_offset // cols)
+
+    def apply_swizzle_v3(self, x, y, rows, cols):
+        #等价V1和V2, 最高效，cutlass的处理
+        offset = x + y * cols
+
+        bit_msk = (1 << self.B) - 1
+        yyy_msk = bit_msk << (self.M + max(0, self.S))
+        msk_sft = self.S
+        if msk_sft > 0:
+            new_offset = offset ^ ((offset & yyy_msk) >> msk_sft)
+        else:
+            new_offset =  offset ^ ((offset & yyy_msk) << -msk_sft)
+        return (new_offset % cols, new_offset // cols)
+
+
 def print_matrix(matrix, title):
     """
     打印矩阵
@@ -45,7 +74,7 @@ assert ROWS * COLS == 2 ** (B + M + S)
 swizzle = Swizzle(B, M, S)
 
 original_matrix = [[(x, y) for x in range(COLS)] for y in range(ROWS)]
-swizzled_matrix = [[swizzle.apply_swizzle(x, y, ROWS, COLS) for x in range(COLS)] for y in range(ROWS)]
+swizzled_matrix = [[swizzle.apply_swizzle_v3(x, y, ROWS, COLS) for x in range(COLS)] for y in range(ROWS)]
 
 print_matrix(original_matrix, "Original Matrix")
 print_matrix(swizzled_matrix, f"Swizzle<B={B}, M={M}, S={S}> Matrix")
