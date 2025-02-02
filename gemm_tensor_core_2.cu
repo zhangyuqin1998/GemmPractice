6015ad170f8c2129f0ac4d1dad61c7ffa7cd5bc2
zhangyuqin1998 <zhangyuqin@baidu.com>
2025-02-03 00:34:15 +0800

    fix code style

--- gemm_tensor_core_2.cu
+++ gemm_tensor_core_2.cu
@@ -10,6 +10,59 @@ __device__ __inline__ void STVectorized(float *dst, const float *src) {
     *(reinterpret_cast<T*>(dst)) = *(reinterpret_cast<const T*>(src));
 }
 
+template <int BBits, int MBase, int SShift = BBits>
+struct Swizzle
+{
+    __device__ __inline__ static uint64_t apply(uint64_t x, uint64_t y, uint64_t rows, uint64_t cols) {
+        uint64_t offset = x + y * cols;
+        uint64_t one = 1;
+        uint64_t bit_msk = (one << BBits) - one;
+        uint64_t yyy_msk = bit_msk << (MBase + SShift);
+        uint64_t msk_sft = SShift;
+        return offset ^ ((offset & yyy_msk) >> msk_sft);;
+    }
+};
+
+
+__device__ __inline__ void cp_async_commit_group() {
+    asm volatile("cp.async.commit_group;\n" ::);
+}
+
+template<int N>
+__device__ __inline__ void cp_async_wait_group() {
+    asm volatile("cp.async.wait_group %0;\n" ::"n"(N));
+}
+
+template<class SrcT>
+__device__ __inline__ void cp_async_128B(uint32_t dst_addr, SrcT src_addr) {
+    constexpr uint32_t nbyte = 16;
+    asm volatile("cp.async.cg.shared.global.L2::128B [%0], [%1], %2;\n" 
+                    ::"r"(dst_addr), "l"(src_addr), "n"(nbyte));
+}
+
+template<class SrcT>
+__device__ __inline__ void ldmatrix_sync_x4_m8n8(
+    uint32_t &r0, uint32_t &r1, uint32_t &r2, uint32_t &r3, SrcT addr
+) {
+    asm volatile("ldmatrix.sync.aligned.x4.m8n8.shared.b16 {%0, %1, %2, %3}, [%4];\n"
+                    : "=r"(r0), "=r"(r1), "=r"(r2), "=r"(r3)
+                    : "l"(addr));
+}
+
+template<class FragmentA, class FragmentB, class FragmentC>
+__device__ __inline__ void mma16x16x16(
+    FragmentA frag_a, FragmentB frag_b, FragmentC frag_c)
+{
+#pragma unroll
+    for (int i = 0; i < 2; ++i) {
+        asm volatile("mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32 {%0,  %1,  %2,  %3},{%4,  %5,  %6,  %7}, {%8,  %9}, {%10, %11, %12, %13};\n"
+                : "=f"(frag_c[i][0]), "=f"(frag_c[i][1]), "=f"(frag_c[i][2]), "=f"(frag_c[i][3])
+                : "r"(frag_a[0]), "r"(frag_a[1]), "r"(frag_a[2]), "r"(frag_a[3]),
+                        "r"(frag_b[i][0]), "r"(frag_b[i][1]),
+                        "f"(frag_c[i][0]), "f"(frag_c[i][1]), "f"(frag_c[i][2]), "f"(frag_c[i][3]));
+    }
+}
+
 __global__ void Kernel(const half *A, const half *B, float *C, uint64_t m, uint64_t n, uint64_t k) {
     const size_t laneid = threadIdx.x % 32;
     
@@ -18,10 +71,10 @@ __global__ void Kernel(const half *A, const half *B, float *C, uint64_t m, uint6
 
     // 要做2次16x8x16才能得到16x16的结果, 但2次用到的A矩阵的数据是一样的
     // 所以FragmentA是一维矩阵即可
-    uint32_t FragmentA[4];
-    uint32_t FragmentB[2][2];
-    float    FragmentC[2][4];
-    memset(FragmentC, 0, sizeof(FragmentC));
+    uint32_t frag_a[4];
+    uint32_t frag_b[2][2];
+    float    frag_c[2][4];
+    memset(frag_c, 0, sizeof(frag_c));
 
 
     uint64_t tiled_m = blockIdx.y * TILE_SIZE;
@@ -33,38 +86,39 @@ __global__ void Kernel(const half *A, const half *B, float *C, uint64_t m, uint6
     uint64_t m_idx = tiled_m + mn_in_tile;
     uint64_t n_idx = tiled_n + mn_in_tile;
 
+    uint64_t st_seizzled_offset = Swizzle<2, 3, 3>::apply(k_in_tile, mn_in_tile, TILE_SIZE, TILE_SIZE);
+    uint64_t ld_swizzled_offset = Swizzle<2, 3, 3>::apply(laneid / 16 * 8, laneid % 16, TILE_SIZE, TILE_SIZE);
+
     for (uint64_t k_tile = 0; k_tile < k; k_tile += TILE_SIZE) {
         uint64_t k_idx = k_tile + k_in_tile;
 
         auto a_src_addr = &A[(m_idx) * k + k_idx];
-        uint32_t sa_dst_addr = __cvta_generic_to_shared(&s_A[mn_in_tile][k_in_tile]);
-        asm volatile("cp.async.cg.shared.global.L2::128B [%0], [%1], %2;\n" ::"r"(sa_dst_addr), "l"(a_src_addr), "n"(16));
+        uint32_t sa_dst_addr = __cvta_generic_to_shared(&s_A[st_seizzled_offset / TILE_SIZE][st_seizzled_offset % TILE_SIZE]);
+        cp_async_128B(sa_dst_addr, a_src_addr);
 
         auto b_src_addr = &B[(n_idx) * k + k_idx];
-        uint32_t sb_dst_addr = __cvta_generic_to_shared(&s_B[mn_in_tile][k_in_tile]);
-        asm volatile("cp.async.cg.shared.global.L2::128B [%0], [%1], %2;\n" ::"r"(sb_dst_addr), "l"(b_src_addr), "n"(16));
+        uint32_t sb_dst_addr = __cvta_generic_to_shared(&s_B[st_seizzled_offset / TILE_SIZE][st_seizzled_offset % TILE_SIZE]);
+        cp_async_128B(sb_dst_addr, b_src_addr);
 
-        asm volatile("cp.async.commit_group;\n" ::);
-        asm volatile("cp.async.wait_group %0;\n" ::"n"(0));
+        cp_async_commit_group();
+        cp_async_wait_group<0>();
 
         __syncthreads();
 
-        asm volatile("ldmatrix.sync.aligned.x4.m8n8.shared.b16 {%0, %1, %2, %3}, [%4];\n"
-                 : "=r"(FragmentA[0]), "=r"(FragmentA[1]), "=r"(FragmentA[2]), "=r"(FragmentA[3])
-                 : "l"(__cvta_generic_to_shared(&s_A[laneid % 16][laneid / 16 * 8])));
+        uint64_t ldmatrix_sa_addr = __cvta_generic_to_shared(&s_A[ld_swizzled_offset / TILE_SIZE][ld_swizzled_offset % TILE_SIZE]);
+        ldmatrix_sync_x4_m8n8(
+            frag_a[0], frag_a[1], frag_a[2], frag_a[3],
+            ldmatrix_sa_addr
+        );
 
-        asm volatile("ldmatrix.sync.aligned.x4.m8n8.shared.b16 {%0, %1, %2, %3}, [%4];\n"
-                 : "=r"(FragmentB[0][0]), "=r"(FragmentB[1][0]), "=r"(FragmentB[0][1]), "=r"(FragmentB[1][1])
-                 : "l"(__cvta_generic_to_shared(&s_B[laneid % 16][laneid / 16 * 8])));
+        uint64_t ldmatrix_sb_addr = __cvta_generic_to_shared(&s_B[ld_swizzled_offset / TILE_SIZE][ld_swizzled_offset % TILE_SIZE]);
+        ldmatrix_sync_x4_m8n8(
+            frag_b[0][0], frag_b[1][0], frag_b[0][1], frag_b[1][1],
+            ldmatrix_sb_addr
+        );
         
-#pragma unroll
-        for (int i = 0; i < 2; ++i) {
-            asm volatile("mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32 {%0,  %1,  %2,  %3},{%4,  %5,  %6,  %7}, {%8,  %9}, {%10, %11, %12, %13};\n"
-                    : "=f"(FragmentC[i][0]), "=f"(FragmentC[i][1]), "=f"(FragmentC[i][2]), "=f"(FragmentC[i][3])
-                    : "r"(FragmentA[0]), "r"(FragmentA[1]), "r"(FragmentA[2]), "r"(FragmentA[3]),
-                            "r"(FragmentB[i][0]), "r"(FragmentB[i][1]),
-                            "f"(FragmentC[i][0]), "f"(FragmentC[i][1]), "f"(FragmentC[i][2]), "f"(FragmentC[i][3]));
-        }
+        mma16x16x16(frag_a, frag_b, frag_c);
+
         __syncthreads();
     }
 
@@ -72,10 +126,10 @@ __global__ void Kernel(const half *A, const half *B, float *C, uint64_t m, uint6
     uint64_t m_in_tile = laneid / 4;
     uint64_t n_in_tile = (laneid % 4) * 2;
 
-    STVectorized<float2>(&C[(tiled_m + m_in_tile) * n + tiled_n + n_in_tile], &FragmentC[0][0]);
-    STVectorized<float2>(&C[(tiled_m + m_in_tile + 8) * n + tiled_n + n_in_tile], &FragmentC[0][2]);
-    STVectorized<float2>(&C[(tiled_m + m_in_tile) * n + tiled_n + n_in_tile + 8], &FragmentC[1][0]);
-    STVectorized<float2>(&C[(tiled_m + m_in_tile + 8) * n + tiled_n + n_in_tile + 8], &FragmentC[1][2]);    
+    STVectorized<float2>(&C[(tiled_m + m_in_tile) * n + tiled_n + n_in_tile], &frag_c[0][0]);
+    STVectorized<float2>(&C[(tiled_m + m_in_tile + 8) * n + tiled_n + n_in_tile], &frag_c[0][2]);
+    STVectorized<float2>(&C[(tiled_m + m_in_tile) * n + tiled_n + n_in_tile + 8], &frag_c[1][0]);
+    STVectorized<float2>(&C[(tiled_m + m_in_tile + 8) * n + tiled_n + n_in_tile + 8], &frag_c[1][2]);    
 }
 
 
